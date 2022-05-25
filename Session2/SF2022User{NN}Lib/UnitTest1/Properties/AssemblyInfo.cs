174c50a981d230b9eac053fe181582d169b1bdf7
cay <cay@mail.ru>
2022-05-26 02:13:34 +0300

    Commit

---
+++ Session2/SF2022User{NN}Lib/UnitTest1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b3022a4f-9274-4a06-a1e7-472ab4676275")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Session2/SF2022User{NN}Lib/UnitTest1/UnitTest1.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using SF2022User_NN_Lib;
+
+namespace UnitTest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+            public void AvailablePeriods_TimeFallse()
+            {
+                SF2022User_NN_Lib.Calculations calculation = new SF2022User_NN_Lib.Calculations();
+
+                
+            }
+        [TestMethod]
+        public void AvailablePeriods_TimeTrue()
+        {
+            SF2022User_NN_Lib.Calculations calculation = new SF2022User_NN_Lib.Calculations();
+
+
+        }
+        [TestMethod]
+        public void AvailablePeriods_TimeFallse2()
+        {
+            SF2022User_NN_Lib.Calculations calculation = new SF2022User_NN_Lib.Calculations();
+
+
+        }
+        [TestMethod]
+        public void AvailablePeriods_TimeFallse3()
+        {
+            SF2022User_NN_Lib.Calculations calculation = new SF2022User_NN_Lib.Calculations();
+
+
+        }
+        [TestMethod]
+        public void AvailablePeriods_TimeFallse4()
+        {
+            SF2022User_NN_Lib.Calculations calculation = new SF2022User_NN_Lib.Calculations();
+
+
+        }
+        [TestMethod]
+        public void AvailablePeriods_TimeFallse5()
+        {
+            SF2022User_NN_Lib.Calculations calculation = new SF2022User_NN_Lib.Calculations();
+
+
+        }
+        [TestMethod]
+        public void AvailablePeriods_TimeFallse6()
+        {
+            SF2022User_NN_Lib.Calculations calculation = new SF2022User_NN_Lib.Calculations();
+
+
+        }
+        [TestMethod]
+        public void AvailablePeriods_TimeFallse7()
+        {
+            SF2022User_NN_Lib.Calculations calculation = new SF2022User_NN_Lib.Calculations();
+
+
+        }
+        [TestMethod]
+        public void AvailablePeriods_TimeFallse8()
+        {
+            SF2022User_NN_Lib.Calculations calculation = new SF2022User_NN_Lib.Calculations();
+
+
+
+        }
+        [TestMethod]
+        public void AvailablePeriods_TimeFallse10()
+        {
+            SF2022User_NN_Lib.Calculations calculation = new SF2022User_NN_Lib.Calculations();
+
+
+        }
+    }
+}
